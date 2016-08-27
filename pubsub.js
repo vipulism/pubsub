e359334e665def2dda7de4c0be628d65c0e357d6
vipulism <vipul0809@gmail.com>
2016-08-27 17:41:48 +0530

    first

---
+++ pubsub.js
@@ -0,0 +1,28 @@
+
+// created by vipul sharma on 27th August 2016
+// Description@ this function is use to subscribe and public things like andgular $emit
+
+var events = {
+  events: {},
+  on: function (eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off: function(eventName, fn) {
+    if (this.events[eventName]) {
+      for (var i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      };
+    }
+  },
+  emit: function (eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+};
